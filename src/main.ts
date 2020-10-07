e0818cb130c3c7dfd4bb11233d557f660a8363e5
Patcharaphorn Lertvirul <kukkik.kpl@gmail.com>
2020-10-07 14:30:23 +0700

    complete

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,32 @@ export function Add(numbers:string): number {
   } 
   if (+numbers) {
     if (isNegative(+numbers)) {
-      throw new Error('Negatives not allowed');
+      throw new Error('Negatives not allowed: '+numbers);
     }
-    return +numbers;
+    return isOver1000(+numbers);
   }
 
-  var delimeter = ','
-
+  var delimeters = []
+  delimeters.push(',')
   if (numbers.startsWith('//')) {
-    delimeter = numbers.slice(2).split('\n')[0]
+    const regex = /(?<=\[)(.*?)(?=\])/g;
+    delimeters = numbers.match(regex);
     numbers = numbers.slice(2).split('\n')[1]
   }
   numbers = numbers.replace('\n', ',');
-  const numberArray = numbers.split(delimeter);
+  const reg = delimeters.join('|').replace(/[<>*()?]/g, "\\$&");
+  const numberArray = numbers.split(new RegExp(reg));
   
   var sum = 0;
+  var negetiveNumbers = [];
   numberArray.forEach(ele => {
     if (isNegative(+ele)) {
-      throw new Error('Negatives not allowed');
+      negetiveNumbers.push(ele)
     }
-    sum += +ele;
+    sum += isOver1000(+ele);
   })
+
+  if (negetiveNumbers.length) throw new Error('Negatives not allowed: '+negetiveNumbers);
     
   return sum;
   
@@ -32,4 +37,8 @@ export function Add(numbers:string): number {
 
 function isNegative(num:number) {
   return num < 0 ? true : false;
+}
+
+function isOver1000(num:number) {
+  return num > 1000 ? 0 : num;
 }
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -28,21 +28,45 @@ describe('Convert string to number', () => {
     expect(num).equal(6);
   });
 
-  it('//***\\n1***2***3 should be equal to 6', () => {
-    const num = Add('//***\n1***2***3');    
+  it('-1 should throw error', () => {
+    try {
+      const num = Add('-1');
+    } catch (err) {
+      expect(err.message).equal('Negatives not allowed: -1');
+    }
+  });
+
+  it('-1,-2,3 should throw error', () => {
+    try {
+      const num = Add('-1,-2,3');
+    } catch (err) {
+      expect(err.message).equal('Negatives not allowed: -1,-2');
+    }
+  });
+
+  it('1000,2 should be equal to 2', () => {
+    const num = Add('1000,2');
+    expect(num).equal(1002);
+  });
+
+  it('1001 should be equal to 0', () => {
+    const num = Add('1001');
+    expect(num).equal(0);
+  });
+
+  it('//[***]\\n1***2***3 should be equal to 6', () => {
+    const num = Add('//[***]\n1***2***3');    
     expect(num).equal(6);
   });
 
-  it('-1 should throw error', () => {
-    // try {
-    //   const num = Add('-1');
-    // } catch (err) {
-    //   console.log(err)
-    //   expect(err).should.throw(Error, /Negatives not allowed/);
-    //   console.log('error')
-    // }
-    
-    // expect(num).to.throw();
+  it('//[*][%]\\n1*2%3 should be equal to 6', () => {
+    const num = Add('//[*][%]\n1*2%3');    
+    expect(num).equal(6);
+  });
+
+  it('//[***][%%%]\\n1***2%%%3 should be equal to 6', () => {
+    const num = Add('//[***][%%%]\n1***2%%%3');    
+    expect(num).equal(6);
   });
 
 });
